f91164ab0dd8e76730ac04b86f1ef31ded3a3207
VinothOfficial <106010245+VinothOfficial@users.noreply.github.com>
2022-05-22 13:56:52 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,37 @@
+var list = [
+    {
+    "Name" : "Karthick",
+    "Year" : "2001",
+    "Country" : "India",
+    "ID" : "0001"
+},
+{
+    "Name" : "Jeeva",
+    "Year" : "2002",
+    "Country" : "India",
+    "ID" : "0002"
+},
+{
+    "Name" : "Surya",
+    "Year" : "2003",
+    "Country" : "India",
+    "ID" : "0003"
+},
+{
+    "Name" : "Arya",
+    "Year" : "2004",
+    "Country" : "India",
+    "ID" : "0004"
+},
+{
+    "Name" : "Dhanush",
+    "Year" : "2005",
+    "Country" : "India",
+    "ID" : "0005"
+},
+];
+    
+print(list[0]["Name"]);
+print(list[0]["ID"]);
+print(list[0]["Year"]);
+print(list[0]["Country"]);
